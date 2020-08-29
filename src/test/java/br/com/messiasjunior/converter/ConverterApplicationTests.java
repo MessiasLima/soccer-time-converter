b1d9f60b1fc0f1a712643b175d575cd93ad8bf08
Messias Junior <messiaslima.03@gmail.com>
2020-08-29 22:19:24 +0100

    chore: application boostrap
    - added spring boot base dependencies
    - set up code style plugin
    - set up commit lint plugin
    - set up git hooks tasks
    - set up code coverage verification

---
+++ src/test/java/br/com/messiasjunior/converter/ConverterApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.messiasjunior.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
