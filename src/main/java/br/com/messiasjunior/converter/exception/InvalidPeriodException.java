01754964f8f887b6bbe516da0558532dc6ca1d73
Messias Junior <messiaslima.03@gmail.com>
2020-08-31 09:41:22 +0100

    feat: period enum validation

--- src/main/java/br/com/messiasjunior/converter/exception/InvalidPeriodException.java
+++ src/main/java/br/com/messiasjunior/converter/exception/InvalidPeriodException.java
@@ -1,4 +1,10 @@
 package br.com.messiasjunior.converter.exception;
 
 public class InvalidPeriodException extends Exception {
+	public InvalidPeriodException() {
+	}
+
+	public InvalidPeriodException(String message) {
+		super(message);
+	}
 }
